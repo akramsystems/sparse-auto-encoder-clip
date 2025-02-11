5e4c9e2b393e777c2b17943f5fdd98b2d0bcc5e3
Ali Akram <akramsystems@gmail.com>
2025-02-11 07:28:44 +0000

    update file paths

--- app.py
+++ app.py
@@ -20,9 +20,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.ba
 layer_index = 11  # As used in training
 input_dim = 768  # CLIP base dimension
 expansion_factor = 64  # Matches training setup
-model_file_path = "/workspaces/sparse-auto-encoder-clip/sae_epoch.pth"
-clip_features_path = "/workspaces/sparse-auto-encoder-clip/clip_features.pt"
-top_activations_path = "/workspaces/sparse-auto-encoder-clip/top_activations.pkl"
+model_file_path = "./sae_epoch.pth"
+assert os.path.exists(model_file_path)
+clip_features_path = "./clip_features.pt"
+assert os.path.exists(clip_features_path)
+top_activations_path = "./top_activations.pkl"
+assert os.path.exists(top_activations_path)
 N_TOP_NEURONS = 10
 N_TOP_IMAGES = 10
 BATCH_SIZE = 32
